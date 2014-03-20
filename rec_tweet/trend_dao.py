6f2327051f85a7525e19bc9e93063c4b16267b70
fcaptuayo <captuayonovoafredy@gmail.com>
2014-03-20 14:19:38 -0500

    Update trend_dao.py

--- rec_tweet/trend_dao.py
+++ rec_tweet/trend_dao.py
@@ -2,19 +2,15 @@
 #Alcamena 10 tendencias en la base de datos.#    
 from twython import Twython, TwythonError
 from trend import Trend
-from status import Status
 from mongoengine import connect
 connect('twitter')
 twitter = Twython("iDGDpOTDVj7mwGDD4pkAMw", "rKOrMHCvsP98lISbyXnHPaR7HFWj44PsAa4Yy7cCQvE", 
     "222814276-KsT3JiNkqUCALLKrfFegVenxi229thKKxjkM2Zpr", "soCtSgfJyzuAEhXvN466LZao7Xc5RcenjFbTmd1TA")
-trends = {}
+
 try:
     search_results = twitter.get_place_trends(id = 23424787)
-except TwythonError as e:
-    print e
-if search_results:
     for trend in search_results[0].get('trends', []):       
         t = Trend(trend)       
-        t.save()                        
-
-        
+        t.save()   
+except TwythonError as e:
+    print e
